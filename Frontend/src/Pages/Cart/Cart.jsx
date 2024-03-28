85d9d340fd1c1fcfd28ded6eb77be2d8d53385d3
Tejas Amol Hande <tejashande143143@gmail.com>
2024-03-28 16:14:42 +0530

    Added OrderPage

--- Frontend/src/Pages/Cart/Cart.jsx
+++ Frontend/src/Pages/Cart/Cart.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../Context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
 
   const {cartItems,food_list,removeFromCart, GetTotalCartAmount} = useContext(StoreContext)
+
+  const Navigate = useNavigate();
+
+
   return (
     <div className="Cart">
       <div className="CartItems">
@@ -44,14 +49,14 @@ const Cart = () => {
             </div>
             <div className="CartTotalDetails">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${GetTotalCartAmount()===0?0:2}</p>
             </div>
             <div className="CartTotalDetails">
               <p>Total</p>
-              <p>${GetTotalCartAmount()+2}</p>
+              <p>${GetTotalCartAmount()===0?0:GetTotalCartAmount()+2}</p>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>Navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="CartPromocode">
           <p>If You Have A Promo Code, Enter It Here</p>
--- Frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import './PlaceOrder.css'
+import React, { useContext } from "react";
+import "./PlaceOrder.css";
+import "../Cart/Cart.css";
+import { StoreContext } from "../../Context/StoreContext";
 
-function PlaceOrder() {
+const PlaceOrder = () => {
+  const { GetTotalCartAmount } = useContext(StoreContext);
+
   return (
-    <div>PlaceOrder</div>
-  )
-}
+    <form className="PlaceOrder">
+      <div className="PlaceOrderLeft">
+        <p className="Title">Delivery Information</p>
+        <div className="MultiFields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="text" placeholder="Email Address" />
+        <input type="text" placeholder="Street" />
+        <div className="MultiFields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="MultiFields">
+          <input type="text" placeholder="Zip Code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Phone" />
+      </div>
+      <div className="PlaceOrderRight">
+        <div className="CartBottom">
+          <div className="CartTotal">
+            <h2>Cart Total</h2>
+            <div>
+              <div className="CartTotalDetails">
+                <p>Subtotal</p>
+                <p>${GetTotalCartAmount()}</p>
+              </div>
+              <div className="CartTotalDetails">
+                <p>Delivery Fee</p>
+                <p>${GetTotalCartAmount() === 0 ? 0 : 2}</p>
+              </div>
+              <div className="CartTotalDetails">
+                <p>Total</p>
+                <p>
+                  ${GetTotalCartAmount() === 0 ? 0 : GetTotalCartAmount() + 2}
+                </p>
+              </div>
+            </div>
+            <button>PROCEED TO PAYMENT</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
