84139a9211c22ba520c36f44b20969577e7826c6
tejhande <59686002+tejhande@users.noreply.github.com>
2024-06-30 17:06:52 +0530

    "Updated Footer, NavBar, and Cart components with new CSS styles and HTML structure."

--- Frontend/src/Pages/Cart/Cart.jsx
+++ Frontend/src/Pages/Cart/Cart.jsx
@@ -31,7 +31,14 @@ const Cart = () => {
                   <p>₹{item.price * 32}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>₹{32 * item.price * cartItems[item._id]}</p>
-                  <p onClick={() => removeFromCart(item._id)} className="Cross">X</p>
+                  <button onClick={() => removeFromCart(item._id)} className="delete-button noselect">
+  <span className="text">Delete</span>
+  <span className="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+    </svg>
+  </span>
+</button>
                 </div>
                 <hr />
               </div>
